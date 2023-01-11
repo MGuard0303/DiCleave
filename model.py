1e56f87c2ac42f34a24483998c4d639bfad01391
MGuard0303 <53183964+MGuard0303@users.noreply.github.com>
2023-01-11 14:40:45 +0900

    Update and rename Model.py to model.py

--- model.py
+++ model.py
@@ -2,6 +2,40 @@ import torch.nn as nn
 import torch
 
 
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.name = "autoencoder"
+
+        self.encoder = nn.Sequential(
+            nn.BatchNorm1d(800),
+            nn.Linear(800, 512),
+            nn.ReLU(),
+            nn.BatchNorm1d(512),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.BatchNorm1d(256),
+            nn.Linear(256, 64),
+            nn.ReLU()
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(64, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 800),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = x.reshape(-1, 800)
+        encoder_out = self.encoder(x)
+        decoder_out = self.decoder(encoder_out)
+
+        return encoder_out, decoder_out, x
+
+
 class CNNModel(nn.Module):
     # noinspection PyTypeChecker
     def __init__(self, name="model", task="binary"):
